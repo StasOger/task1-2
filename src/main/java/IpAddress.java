54bfe342c9caa68a39b14353975f46fd4e6c97a2
StasOger <92465858+StasOger@users.noreply.github.com>
2023-04-05 09:55:11 +0300

    Initial commit

---
+++ src/main/java/IpAddress.java
@@ -0,0 +1,41 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IpAddress {
+    //Задание 1 "IP-адреса"
+//        IpV4 адрес состоит из 4 октетов, значит его можно хранить в переменной типа int32.
+//        Реализуйте пару функций, которые однозначно преобразуют строковое представление IpV4 адрес (вида "127.0.0.1") в значение типа int32 и наоборот.
+//
+//        Примеры:
+//        2149583360 ==> "128.32.10.0"
+//        255        ==> "0.0.0.255"
+
+    public static void ipv4ToInt32(String IpV4){
+        String[] massiveIpV4 = IpV4.split("\\.");
+        long[] massIpV4 = new long[massiveIpV4.length];
+        for (int i = 0; i<massiveIpV4.length; i++){
+            massIpV4[i] = Long.parseLong(massiveIpV4[i]);
+        }
+        long int32 = (long) (massIpV4[0]*Math.pow(2,24)+massIpV4[1]*Math.pow(2,16)+massIpV4[2]*Math.pow(2,8)+massIpV4[3]);
+        System.out.println("IpV4  ->  int32    " + IpV4 + " -> " + int32);
+    }
+    public static void int32ToIpV4(long ipaddr) throws UnknownHostException {
+        byte[] addr = new byte[4];
+        addr[0] = (byte) ((ipaddr >> 24) & 0xFF);
+        addr[1] = (byte) ((ipaddr >> 16) & 0xFF);
+        addr[2] = (byte) ((ipaddr >> 8 ) & 0xFF);
+        addr[3] = (byte) (ipaddr & 0xFF);
+        InetAddress inetAddr = InetAddress.getByAddress(addr);
+        String inetAdressString = inetAddr.toString().replaceAll("/", "");
+
+        System.out.println("int32  ->  IpV4    " + ipaddr + " -> " + inetAdressString);
+    }
+    public static void main(String[] args) throws UnknownHostException {
+        ipv4ToInt32("128.32.10.0");
+        ipv4ToInt32("0.0.0.255");
+        ipv4ToInt32("999.999.99.999");
+        int32ToIpV4(2142583360);
+        int32ToIpV4(255);
+        int32ToIpV4(914952360);
+    }
+}
---
+++ src/main/java/TowardsZeroOrInfinity.java
@@ -0,0 +1,51 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class TowardsZeroOrInfinity {
+//Задание 2 "Стремится к нулю или к бесконечности?"
+//  (n! обозначает factorial(n))
+//  Будет ли выражение вида:
+//    un = (1 / n!) * (1! + 2! + 3! + ... + n!)
+//  стремится к 0 или к бесконечности?
+//  Реализуйте функцию, которая расчитывает значение un для целочисленных n > 1 (с точностью не хуже 6 знаков после запятой).
+
+    public static long getFactorial(long f) {
+        if (f <= 1) {
+            return  1;
+        }
+        else {
+            return f * getFactorial(f - 1);
+        }
+    }
+
+    public static void getUn(long n){
+// Пусть un = y * x
+// Находим y
+        BigDecimal y = new BigDecimal(1.0/getFactorial(n));
+        BigDecimal x = new BigDecimal(0);
+        for (int i=1; i<=n; i++){
+// Находим x
+            x = x.add(BigDecimal.valueOf(getFactorial(i)));
+        }
+// Находим un
+        BigDecimal un = y.multiply(x);
+
+// Вывод ответа (un) в консоль
+        System.out.println("При n = "+ n + ", un = "+ un);
+    }
+
+    public static void main(String[] args) {
+//Пример
+        System.out.println("Пример");
+        getUn(2);
+        getUn(5);
+        getUn(65);
+
+        System.out.println();
+        System.out.println("Введите число n (n > 1): ");
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        getUn(n);
+    }
+}
